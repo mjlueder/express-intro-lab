b74903de2bdfc8260f22983e929c0fbef7d53aa0
Mary Lueder <mlueder@gmail.com>
2023-01-03 18:46:35 -0500

    add gorillas data & view

---
+++ data/gorilla-data.js
@@ -0,0 +1,11 @@
+const gorillas = [
+  {name: 'Kashata' , age: 28, gender: 'F', personality: 'tiny and takes no shit from anyone.'},
+  {name: 'Gus', age: 41, gender: 'M', personality: 'distinguished, sweet, and grumpy. The best gorilla.'},
+  {name: 'Grace', age: 2, gender: 'F', personality: 'an adorable terror'},
+  {name: 'Gino', age: 42, gender: 'M', personality: 'a goofy sweetheart/punk'},
+  {name: 'Azizi', age: 23, gender: 'F', personality: 'brilliant and ornary'}
+]
+
+export {
+  gorillas
+}
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 import express from 'express'
 
+import { gorillas } from './data/gorilla-data.js'
+
 // Create Express app
 
 const app = express()
@@ -17,11 +19,17 @@ app.set('view engine', 'ejs')
 // Mount routes
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello, friend</h1>')
+  res.redirect('/home')
 })
 
 app.get('/home', function(req, res){
   res.render('home')
+})
+
+app.get('/gorillas', function(req, res){
+  res.render('gorillas/index', {
+    gorillas: gorillas
+  })
 })
 
 // Tell the app to listen on port 3000
